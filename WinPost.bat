18269f29c5075a73c4896fc460d3cfbd97fb00f6
TheProtyro <protyro@protonmail.com>
2022-05-29 04:58:16 -0400

    Update WinPost.bat

--- WinPost.bat
+++ WinPost.bat
@@ -1,37 +1,68 @@
+echo @off
 (
-echo "------- USER INFO -------"
-
-
+echo.
+echo ------- SYSTEM INFO -------
+echo.
+systeminfo
+echo.
+echo [+] Volumes
+echo.
+mountvol
+echo.
+echo [+] Shares
+echo.
+net share
+echo.
+echo [+] Environment variable
+echo.
+set
+echo.
+echo ------- USER INFO -------
+echo.
 echo %username% || whoami /all
-
-
-echo "[+] Local users"
+echo.
+echo [+] Local users
+echo.
 net user
-
-
-echo "[+] Local policy"
+echo.
+echo [+] Local policy
+echo.
 net accounts
-
-
-echo "[+] Local groups"
+echo.
+echo [+] Local groups
+echo.
 net localgroup
-
-
-echo "------- NETWORK INFO -------"
-
-
-echo "[+] IP configuration"
+echo.
+echo ------- NETWORK INFO -------
+echo.
+echo [+] IP configuration
+echo.
 ipconfig /all
-
-
-echo "[+] Routing table"
+echo.
+echo [+] Routing table
+echo.
 route print
-
-
-echo "[+] ARP table"
+echo.
+echo [+] ARP table
+echo.
 arp -a
-
-echo "[+] Connections"
+echo.
+echo [+] Connections
+echo.
 netstat -ano
-
+echo.
+echo [+] Wifi
+echo.
+netsh wlan show profile
+echo.
+echo ------- PROCESS INFO -------
+echo.
+echo [+] Services
+echo.
+tasklist /svc
+echo.
+echo [+] Scheduled tasks
+echo.
+schtasks /query /fo LIST /v
+echo.
 ) > %COMPUTERNAME%.txt 
